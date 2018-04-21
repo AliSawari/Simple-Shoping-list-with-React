481e7fc305b65d17c5cdf426a980097e43c86185
mrghost <ali.dragon.swr@gmail.com>
2018-04-21 19:40:03 +0430

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
+// module.exports = {
+//     entry:'./src/app.js',
+//     output:{
+//         filename:'b.js',
+//         path:'/home/mrmzr/Desktop/ReactEx/dist'
+//     },
+//     module:{
+//         rules:[
+//             {
+//                 test:/\.js$/,
+//                 exclude:/node_modules/,
+//                 use:[
+//                     'babel-loader'
+//                 ],
+//             }
+//         ]
+//     }
+// }
+
 module.exports = {
-    entry:'./src/app.js',
-    output:{
-        filename:'b.js',
-        path:'/home/mrmzr/Desktop/ReactEx/dist'
-    },
-    module:{
-        rules:[
-            {
-                test:/\.js$/,
-                exclude:/node_modules/,
-                use:[
-                    'babel-loader'
-                ],
-            }
-        ]
-    }
+  entry: './src/app.js',
+  output: {
+    path: __dirname,
+    filename: "./dist/b.js"
+  },
+  resolve: {
+    extensions:[".js",".jsx"]
+  },
+  module: {
+    rules: [
+      {
+       exclude: /(node_modules)/,
+       use: ['babel-loader']
+      }
+    ]
+  }
 }
